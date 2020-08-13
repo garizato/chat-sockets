9c837f7eeb77cc54ca9cdfbe47ec9bc903d0a60e
garizato <garizato@gmail.com>
2020-08-12 22:58:05 -0500

    Chat con sockets

---
+++ server/classes/usuarios.js
@@ -0,0 +1,44 @@
+class Usuarios {
+
+    constructor() {
+        this.integrantes = [];
+    }
+
+    //Agregar un integrante al chat
+    addIntegrante(id, nombre, sala) {
+        let integrante = { id, nombre, sala };
+
+        this.integrantes.push(integrante);
+
+        return this.integrantes;
+    }
+
+    //Consultar un integrante
+    getIntegrante(id) {
+        let integrante = this.integrantes.filter(integrante => integrante.id === id)[0];
+
+        return integrante;
+    }
+
+    getIntegrantes() {
+        return this.integrantes;
+    }
+
+    getIntegrantesxSala(sala) {
+        return this.integrantes.filter(integrante => integrante.sala === sala)
+    }
+
+    deleteIntegrante(id) {
+
+        let integranteDeleted = this.getIntegrante(id);
+
+        this.integrantes = this.integrantes.filter(integrante => integrante.id != id);
+
+        return integranteDeleted;
+    }
+
+}
+
+module.exports = {
+    Usuarios
+};
---
+++ server/sockets/socket.js
@@ -0,0 +1,49 @@
+const { io } = require('../server');
+const { Usuarios } = require('../classes/usuarios');
+const { crearMensaje } = require('../utils/utils');
+
+const usuarios = new Usuarios();
+
+io.on('connection', (client) => {
+
+    client.on('entrarChat', (peticion, callback) => {
+
+        if (!peticion.nombre || !peticion.sala) {
+            return callback({
+                ok: false,
+                message: "El nombre y la sala son necesarios"
+            });
+        }
+
+        client.join(peticion.sala);
+
+        usuarios.addIntegrante(client.id, peticion.nombre, peticion.sala);
+
+        client.broadcast.to(peticion.sala).emit('listaIntegrantes', usuarios.getIntegrantesxSala(peticion.sala));
+
+        callback(usuarios.getIntegrantesxSala(peticion.sala));
+    });
+
+
+    client.on('crearMensaje', (data) => {
+        let integrante = usuarios.getIntegrante(client.id);
+
+        let mensaje = crearMensaje(integrante.nombre, data.mensaje);
+        client.broadcast.to(integrante.sala).emit('crearMensaje', mensaje);
+    });
+
+    client.on('disconnect', () => {
+        let integranteDeleted = usuarios.deleteIntegrante(client.id);
+        client.broadcast.to(integranteDeleted.sala).emit('crearMensaje', crearMensaje('Administrador', `${ integranteDeleted.nombre } abandonó el chat`));
+        client.broadcast.to(integranteDeleted.sala).emit('listaIntegrantes', usuarios.getIntegrantesxSala(integranteDeleted.sala));
+    });
+
+    //Mensajes Privados
+    client.on('mensajePrivado', data => {
+
+        let integrante = usuarios.getIntegrante(client.id);
+
+        client.broadcast.to(data.destinatario).emit('mensajePrivado', crearMensaje(integrante.nombre, data.mensaje));
+    });
+
+});
